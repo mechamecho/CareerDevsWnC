c12fe9e6f4c5d80cc1bf3e6aede1882bbbfc1a07
Nafissa Hassan <nafissa_92@hotmail.com>
2017-07-15 15:06:21 +0000

    Version 1 - Arrays

---
+++ Version1-Arrays.js
@@ -0,0 +1,12 @@
+//Requirement 1: a place to store the todos
+var todos=['item1', 'item2', 'item3', 'item4'];
+//Requirement 2: a way to display todos
+console.log(todos);
+//Requirement 3: A way to add todos:
+todos.push('new item');
+//Requirement 4: a way to change a todo:
+var index_of_item_to_be_changed;
+todos[index_of_item_to_be_changed]='new value';
+//Requirement 5: a way to remove a todo:
+var index_of_item_to_be_removed;
+todos.splice(index_of_item_to_be_removed,1);
