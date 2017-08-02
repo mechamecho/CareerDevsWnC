e49d85104a29a62bb8167f5bcd7668b38e115b1c
Nafissa Hassan <nafissa_92@hotmail.com>
2017-08-02 23:19:24 +0000

    Finish Version11

--- script.js
+++ script.js
@@ -26,25 +26,33 @@ var todoList={
         var totalTodos=this.todos.length;
         var completedTodos=0;
         //Get number of completed todos
-        for(var i=0; i<this.todos.length;i++){
-            if(this.todos[i].completed===true){
-                completedTodos++;
-            }
-        }
+        this.todos.forEach(function(todo){
+           if(todo.completed===true){
+               completedTodos++;
+           } 
+        });
         
         //If everything is true, make everything false
-        if(completedTodos===totalTodos){
-            //Make everything false
-            for (var i=0;i<totalTodos; i++){
-                this.todos[i].completed=false;
+        // if(completedTodos===totalTodos){
+        //     //Make everything false
+        //     this.todos.forEach(function(todo){
+        //       todo.completed=false; 
+        //     });
+        //     //Otherwise make everything true
+        // }else{
+        // this.todos.forEach(function(todo){
+        //     todo.completed=true;
+        // })
+        // }
+        this.todos.forEach(function(todo){
+            //Case1: if everything's true, make everything false
+            if(completedTodos===totalTodos){
+                todo.completed=false;
+            //Case2: make everything true
+            }else{
+                todo.completed=true;
             }
-            //Otherwise make everything true
-        }else{
-           for (var i=0;i<totalTodos; i++){
-                this.todos[i].completed=true;
-            } 
-        }
-        this.displayTodos();
+        })
     }
 };
 
@@ -84,10 +92,27 @@ var view={
     displayTodos:function(){
         var todosUl=document.querySelector("ul");
         todosUl.innerHTML='';
-        for (var i=0; i<todoList.todos.length;i++){
+        // for (var i=0; i<todoList.todos.length;i++){
 
+        //     var todoLi=document.createElement('li');
+        //     var todo=todoList.todos[i];
+        //     var todoTextWithCompletion='';
+        //     //create string for to do and completion depending on todos completion status
+        //     if (todo.completed===true){
+        //         todoTextWithCompletion="(X)"+todo.todoText;
+        //     }else{
+        //         todoTextWithCompletion="( )"+todo.todoText;    
+        //     }
+        //     //to give each li element the to do position as an id
+        //     todoLi.id=i;
+        //     //insert text into li element
+        //     todoLi.textContent=todoTextWithCompletion;
+        //     todoLi.appendChild(this.createDeleteButton());
+        //     todosUl.appendChild(todoLi); 
+            
+        // }
+        todoList.todos.forEach(function(todo,position){
             var todoLi=document.createElement('li');
-            var todo=todoList.todos[i];
             var todoTextWithCompletion='';
             //create string for to do and completion depending on todos completion status
             if (todo.completed===true){
@@ -96,13 +121,12 @@ var view={
                 todoTextWithCompletion="( )"+todo.todoText;    
             }
             //to give each li element the to do position as an id
-            todoLi.id=i;
+            todoLi.id=position;
             //insert text into li element
             todoLi.textContent=todoTextWithCompletion;
             todoLi.appendChild(this.createDeleteButton());
-            todosUl.appendChild(todoLi); 
-            
-        }
+            todosUl.appendChild(todoLi);    
+        }, view)
     },
     createDeleteButton:function(){
         var deleteButton=document.createElement("button");
